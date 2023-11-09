63d15518113dc0469cc49193c9781a419c7402da
Shematic02 <shematic02@mail.ru>
2023-11-09 16:11:34 +0300

    Ex002

---
+++ Ex002/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" 2122 Hello World!");
+        }
+    }
+}
